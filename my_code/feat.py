9ed70aff9fbb75f6babd9216aa3b85f6b20b04ec
ehud_gordon <ehud.gordon@mail.huji.ac.il>
2021-04-17 16:08:50 +0300

    Q1.6-Q1.7

--- my_code/feat.py
+++ my_code/feat.py
@@ -37,14 +37,14 @@ def match_images(img1, img2, sig=True, to_plot=True):
     kp1, desc1 = orb.detectAndCompute(img1, None) # query
     kp2, desc2 = orb.detectAndCompute(img2, None) # train
     img_name = f'{orb.getDefaultName()}'
-    if sig:
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        # img_name += '_sig'
-    else:
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-        # img_name += '_no_sig'
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=sig)
     matches = bf.match(desc1, desc2)
     matches = sorted(matches, key=lambda x: x.distance)
+    pts1 = np.zeros((2,len(matches)))
+    pts2 = np.zeros((2,len(matches)))
+    for i, match in enumerate(matches):
+        pts1[:,i] =  kp1[match.queryIdx].pt
+        pts2[:, i] = kp2[match.trainIdx].pt
     # plot incorrect
     # color = CYAN_COLOR
     # img_name += '_incorrect'
@@ -59,11 +59,48 @@ def match_images(img1, img2, sig=True, to_plot=True):
     #     if len(incorrect_mathces)>50:
     #         break
     if to_plot:
-        img_name+='_correct'
-        match_img = cv2.drawMatches(img1, kp1, img2, kp2, matches[0:30], None,matchColor=ORANGE_COLOR, flags=2)
+        match_img = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=2)
         plt_disp_img(img=match_img, name=img_name, save=True)
+    return matches, pts1, pts2
 
-    return matches
+def read_cameras():
+    # read camera matrices
+    with open(os.path.join(DATA_PATH,'calib.txt')) as f:
+        l1 = f.readline().split()[1:]
+        l2 = f.readline().split()[1:]
+    m1 = np.array([float(i) for i in l1]).reshape(3,4)
+    m2 = np.array([float(i) for i in l2]).reshape(3,4)
+    k = m1[:, :3]
+    m1 = np.linalg.inv(k) @ m1
+    m2 = np.linalg.inv(k) @ m2
+    return k, m1,m2
+
+
+def triangulate(pts1, pts2, m1, m2):
+    num_points = pts1.shape[1]
+    new_points = np.zeros((len(matches), 4))
+    p1,p2,p3 = m1
+    p1_, p2_, p3_ = m2
+    for i in range(num_points):
+        x,y = pts1[:,i]
+        x_,y_ = pts2[:, i]
+        A = np.vstack((
+            [p3*x - p1],
+            [p3*y - p2],
+            [p3_*x_- p1_],
+            [p3_*y_ - p2_]
+        ))
+        u,s,vh = np.linalg.svd(A)
+        X = vh[2,:] # (4,)
+        for j in range(50):
+            first_eq = X @ p3
+            second_eq = X @ p3_
+            A = np.vstack((A[:2]*first_eq, A[2:]*second_eq))
+            u, s, vh = np.linalg.svd(A)
+            X = vh[2, :]  # (4,)
+        new_points[i] = X.T
+    inhom_points = new_points[:,:-1] / (new_points[:,-1].reshape(-1,1)) # (n,3)
+    return inhom_points
 
 
 if __name__=="__main__":
@@ -71,7 +108,24 @@ if __name__=="__main__":
     # Q1.1- Q1.2
     # kp1 = draw_kp(img=img1, to_plot=True)
     # kp2 = draw_kp(img=img2, to_plot=True)
-    match_images(img1, img2, sig=True)
+    matches, pts1, pts2 = match_images(img1, img2, sig=True, to_plot=False) # list(n), (2Xn), (2xn)
+    k, m1, m2 = read_cameras() #k=(3,3) m1,m2 (3,4)
+    inhom_points = triangulate(pts1,pts2, m1, m2) # (n,3)
+    cv2_points = cv2.triangulatePoints(projMatr1=m1, projMatr2=m2, projPoints1=pts1, projPoints2=pts2) # (4,n)
+    cv2_inhom = cv2_points[:-1] / (cv2_points[-1].reshape(1, -1)) # (3,n)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(inhom_points[:,0],inhom_points[:,1],inhom_points[:,2])
+    ax.set_title('mine')
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.set_title('cv2')
+    ax.scatter(cv2_inhom[0,:], cv2_inhom[1,:], cv2_inhom[2,:])
+    plt.show()
+    print('end')
 
 
 
