3ecbca2232986fa4198c75ff73d8121b0497801a
ehud_gordon <ehud.gordon@mail.huji.ac.il>
2021-05-19 17:22:51 +0300

    stage 2

--- my_code/drive.py
+++ my_code/drive.py
@@ -1,14 +1,15 @@
 import os
+import time
 
 import cv2
 import numpy as np
-import my_code.utils as utils
-import kitti
-import features
-import triang
-from pnp import PNP
 import matplotlib.pyplot as plt
-import time
+
+import my_code.utils as utils
+import kitti, features, triang, tracks
+from pnp import PNP
+
+
 
 class Drive:
     def __init__(self, dataset_path=kitti.DATASET_5_PATH):
@@ -20,28 +21,27 @@ class Drive:
         keep_trying = True
         # match l0-r0, l1-r1
         pnp = PNP(k=self.k, ext_r0=self.ext_r0)
-        seq_length = kitti.get_seq_length(dataset_path=self.dataset_path)
-        # seq_length = 10
+        seq_length = kitti.get_seq_length(dataset_path=self.dataset_path) # 2760
+        seq_length = 3
+        save = False; plot = False
 
         while keep_trying:
-            total_mat_dist = 0.
             # match l0-r0, l1-r1
             kp_l0, desc_l0, kp_r0 = features.get_kps_desc_stereo_pair(idx=0)
+            tracks_db = tracks.Tracks(kp_l0=kp_l0, kp_r0=kp_r0)
             ext_l0 = self.ext_l0
             l1_dws = []; l1_dws_pose = []
-            total_mat_dist = 0.
-            mat_dists = []
+            left1_ext_dists = []
             tx_error, ty_error, tz_error = [], [], []
             start = time.time()
-            kp_l0_inlier_matches = []
-            # global_ext_l_i = self.ext_l0 # (4,4)
+            kp_l1_inlier_matches = []
+            # global_ext_l_i = self.ext_l0 # (4,4) # local
 
 
             keep_trying = False
-            for l1_idx in range(1,seq_length):
+            for l1_idx in range(1,seq_length+1):
                 pc_l0_r0 = triang.triang(kp1=kp_l0, kp2=kp_r0, k=self.k, m1=ext_l0, m2=(self.ext_r0 @ ext_l0)) # (3,n) # global
                 # pc_l0_r0 = triang.triang(kp1=kp_l0, kp2=kp_r0, k=self.k, m1=self.ext_l0, m2=self.ext_r0)  # (3,n) # local
-
                 kp_l1, desc_l1, kp_r1 = features.get_kps_desc_stereo_pair(idx=l1_idx)
 
                 # match l0-l1
@@ -49,37 +49,32 @@ class Drive:
                 # query is 0 side, train is 1 side. The holy grail
                 matches_l0_l1 = features.filter_knn_matches(knn_matches_l0_l1, kp1=kp_l0, kp2=kp_l1, stereo_filter=False)
                 # filt_l1_l0 = {m.trainIdx: m.queryIdx for m in matches_l0_l1}
-
+                tracks_db.add_frame(matches_l0_l1=matches_l0_l1, l1_idx=l1_idx, kp_l1=kp_l1, kp_r1=kp_r1)
                 # get ext_l1 from RANSAC
                 pnp.set_with_matches(matches_l0_l1=matches_l0_l1, kp_l0=kp_l0, kp_l1=kp_l1, pc_l0_r0=pc_l0_r0, kp_r1=kp_r1)
                 ext_l1, ext_l1_bool = pnp.pnp_ransac(iters=100)  # (4,4)
-                # global_ext_l_i = ext_l1 @ global_ext_l_i
+                # global_ext_l_i = ext_l1 @ global_ext_l_i # local
 
-                kp_l0_inlier_matches.append((kp_l0.shape[1], sum(ext_l1_bool), len(matches_l0_l1)))
+                kp_l1_inlier_matches.append((kp_l1.shape[1], sum(ext_l1_bool), len(matches_l0_l1)))
                 # l1 camera place in l0 coordinates
                 l1_dw = utils.get_dw_from_extrinsics(ext_l1) # (3,1) global
                 # l1_dw = utils.get_dw_from_extrinsics(global_ext_l_i) # (3,1) local
                 l1_dws.append(l1_dw)
                 # pose
                 ext_l1_pose = kitti.read_poses_world_to_cam([l1_idx])[0]  # FROM WORLD (l0) TO CAMERA (l1), (4,4)
-                tx_error.append(abs(ext_l1_pose[0,3] - ext_l1[0,3]));ty_error.append(abs(ext_l1_pose[1,3] - ext_l1[1,3]));tz_error.append(abs(ext_l1_pose[2,3] - ext_l1[2,3])) # local
-                # tx_error.append(abs(ext_l1_pose[0, 3] - global_ext_l_i[0, 3])); ty_error.append(abs(ext_l1_pose[1, 3] - global_ext_l_i[1, 3]));tz_error.append(abs(ext_l1_pose[2, 3] - global_ext_l_i[2, 3])) # global
+
                 l1_dw_pose = utils.get_dw_from_extrinsics(ext_l1_pose) # (3,1)
                 l1_dws_pose.append(l1_dw_pose)
-                mat_dist = np.sum(abs(ext_l1_pose-ext_l1)); mat_dists.append(mat_dist) # global
-                # mat_dist = np.sum(abs(ext_l1_pose - global_ext_l_i)); mat_dists.append(mat_dist) # local
-                print(f"{l1_idx}: {kp_l0_inlier_matches[-1]}, mat_dist:{mat_dist:.2f}")
 
-                total_mat_dist += mat_dist
-                # if l1_idx >= 1:
-                #     filt_kp_l0 = pnp.kp_l0
-                #     filt_kp_l1 = pnp.kp_l1
-                #     # plot_inliers_outliers_of_ext1(idx_of_l0=i-1,filt_kp_l0=filt_kp_l0, filt_kp_l1=filt_kp_l1, ext_l1_inliers_bool=ext_l1_bool)
-                #     draw_inliers_double(l0_idx=l1_idx-1, kp1=filt_kp_l0, kp2=filt_kp_l1,inliers=ext_l1_bool, pnp=pnp)
-                #     # draw_two_matches(i-1)
-                if mat_dist > 700:
+                tx_error.append(abs(ext_l1_pose[0, 3] - ext_l1[0, 3])); ty_error.append(abs(ext_l1_pose[1, 3] - ext_l1[1, 3])); tz_error.append(abs(ext_l1_pose[2, 3] - ext_l1[2, 3]))  # global
+                # tx_error.append(abs(ext_l1_pose[0, 3] - global_ext_l_i[0, 3])); ty_error.append(abs(ext_l1_pose[1, 3] - global_ext_l_i[1, 3]));tz_error.append(abs(ext_l1_pose[2, 3] - global_ext_l_i[2, 3])) # local
+                left1_ext_dist = np.sum(abs(ext_l1_pose-ext_l1)); left1_ext_dists.append(left1_ext_dist) # global
+                # left1_ext_dists = np.sum(abs(ext_l1_pose - global_ext_l_i)); left1_ext_dists.append(left1_ext_dist) # local
+                print(f"{l1_idx}: {kp_l1_inlier_matches[-1]}, left1_ext_dist:{left1_ext_dist:.2f}")
+
+                if left1_ext_dist > 700:
                     keep_trying = True
-                    print(f"l1_idx={l1_idx}, mat dist too large: {mat_dist:.0f}")
+                    print(f"l1_idx={l1_idx}, mat dist too large: {left1_ext_dist:.0f}")
                     break
 
                 ext_l0 = ext_l1
@@ -89,37 +84,40 @@ class Drive:
                 if l1_idx % 20 == 0:
                     print(f'finished frame {l1_idx}')
 
-        end = time.time()
-        elapsed_time = end-start; avg_time = elapsed_time / seq_length
-        print(f'elapsed: {elapsed_time:.3f} sec, avg_per_iter={avg_time:.0f} sec')
-        print(f"avg_mat_dist={total_mat_dist/seq_length:.0f} when creating pc with triangulation")
+        tracks_db.serialize()
+        elapsed_time = time.time()-start; avg_time = elapsed_time / seq_length
+        print(f'elapsed: {elapsed_time:.0f} sec, avg_per_frame={avg_time:.1f} sec')
+        print(f"avg_left1_ext_dist={sum(left1_ext_dists)/len(left1_ext_dists):.0f} when creating pc with triangulation")
         print(f"tx_error:{sum(tx_error):.0f}"); print(f"ty_error:{sum(ty_error):.0f}"); print(f"tz_error:{sum(tz_error):.0f}")
         # for l1_dw, l1_dw_pose in zip(l1_dws, l1_dws_pose):
         #     print(l1_dw); print(l1_dw_pose); print()
-        l1_dws = np.array(l1_dws).T # (3, seq_length)
-        l1_dws_pose = np.array(l1_dws_pose).T  # (3,seq_length)
+        if not plot:
+            return
+        l1_dws = np.array(l1_dws).T # (3, seq_length-1)
+        l1_dws_pose = np.array(l1_dws_pose).T  # (3,seq_length-1)
 
-        # plot mat_dists as per frame
-        rng = np.arange(seq_length-1)+1
-        plt.figure(); plt.plot(rng, mat_dists); plt.xlabel('frame'); plt.ylabel('L1 Norm btwn extrinsics')
+        # plot left1_ext_dists per frame
+        rng = np.arange(seq_length)+1 # [1-2760]
+        plt.figure(); plt.plot(rng, left1_ext_dists); plt.xlabel('frame'); plt.ylabel('L1 Norm btwn extrinsics')
         plt.title("L1 Norm btwn my global left_i extrinsics and pose left_i extrinsics")
-        path = os.path.join(utils.FIG_PATH, f'ext_mat_dist_{seq_length}' + '.png'); path = utils.get_avail_path(path); plt.savefig(path, bbox_inches='tight', pad_inches=0)
+        if save:
+            path = os.path.join(utils.FIG_PATH, f'left1_ext_dist_{seq_length}' + '.png'); path = utils.get_avail_path(path); plt.savefig(path, bbox_inches='tight', pad_inches=0)
         plt.show()
 
-        # plot tx, ty, tz error
+        # plot tx, ty, tz L1 distance
         plt.figure(); plt.plot(rng, tx_error, label="tx"); plt.plot(rng, ty_error, label="ty"); plt.plot(rng, tz_error, label="tz")
         plt.legend(); plt.xlabel('frame'); plt.ylabel('L1 Norm')
         plt.title("L1 Norm btwn translation vector of my left_i extrinsic and pose left_i extrinsic")
         path = os.path.join(utils.FIG_PATH, f'trans_vectors_dist_{seq_length}' + '.png'); path = utils.get_avail_path(path); plt.savefig(path, bbox_inches='tight', pad_inches=0)
         plt.show()
 
-        # plot number of kp, inlier matches
+        # plot number of kp, inlier and matches
         fig = plt.figure()
-        plt.plot(np.arange(seq_length-1), [t[0] for t in kp_l0_inlier_matches], label="kp_left0")
-        plt.plot(np.arange(seq_length-1), [t[1] for t in kp_l0_inlier_matches], label="inlier");plt.plot(np.arange(seq_length-1), [t[2] for t in kp_l0_inlier_matches], label="matches")
+        plt.plot(rng, [t[0] for t in kp_l1_inlier_matches], label="kp_left0")
+        plt.plot(rng, [t[1] for t in kp_l1_inlier_matches], label="inlier");plt.plot(rng, [t[2] for t in kp_l1_inlier_matches], label="matches")
         plt.xlabel('frame'); plt.ylabel('count'); plt.legend(); plt.show()
 
-        # plot 2d l1_ camera location comparison
+        # plot 2d left_i camera location comparison
         fig = plt.figure()
         plt.scatter(x=l1_dws[2], y=l1_dws[0], color="blue", label="mine")
         plt.scatter(x=l1_dws_pose[2], y=l1_dws_pose[0], color="red", label="pose",alpha=0.4)
@@ -131,7 +129,7 @@ class Drive:
         plt.savefig(path, bbox_inches='tight', pad_inches=0)
         plt.show()
 
-        # plot 3D l1_camera comparison
+        # plot 3D left1_camera comparison
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         ax.scatter(l1_dws[0], l1_dws[2], l1_dws[1], color="blue", label="mine")
---
+++ my_code/tracks.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+import os
+import pickle
+from time import gmtime, strftime
+TRACKS_PATH = r'C:\Users\godin\Documents\VAN_ex\tracks'
+
+class Tracks:
+    def __init__(self, kp_l0, kp_r0,first_frame_ind=0, td=None):
+        self.first_frame_ind = first_frame_ind
+        if td is None:
+            self.td = dict()
+            self.td[first_frame_ind] = dict()
+        else:
+            self.td = td
+        self.kp_l0 = kp_l0.astype(int)
+        self.kp_r0 = kp_r0.astype(int)
+
+
+    def add_frame(self, matches_l0_l1, l1_idx, kp_l1, kp_r1):
+        kp_l1 = kp_l1.astype(int); kp_r1 = kp_r1.astype(int)
+        l0_idx = l1_idx-1
+        self.td[l1_idx] = dict()
+        for m in matches_l0_l1:
+            l0_m_idx = m.queryIdx
+            l1_m_idx = m.trainIdx
+            # add all matches to l1
+            if l0_m_idx in self.td[l0_idx]:
+                track_id = self.td[l0_idx][l0_m_idx][0]
+                track_length = self.td[l0_idx][l0_m_idx][3]
+                self.td[l1_idx][l1_m_idx]= [track_id, tuple(kp_l1[:,l1_m_idx]), tuple(kp_r1[:,l1_m_idx]), track_length+1]
+            else:
+                track_id = (l0_idx, l0_m_idx)
+                self.td[l0_idx][l0_m_idx] = [track_id, tuple(self.kp_l0[:,l0_m_idx]), tuple(self.kp_r0[:, l0_m_idx]),1]
+                self.td[l1_idx][l1_m_idx] = [track_id, tuple(kp_l1[:, l1_m_idx]), tuple(kp_r1[:, l1_m_idx]),2]
+        self.kp_l0 = kp_l1
+        self.kp_r0 = kp_r1
+
+
+    def get_track_ids(self, camera_id):
+        try:
+            return [l[0] for l in self.td[camera_id].values()] # list of lists
+        except KeyError:
+            return None
+
+    def get_pixels(self,caemra_id, track_id):
+        try:
+            return self.td[caemra_id][track_id][2:4]
+        except KeyError:
+            return None
+    
+    def get_track(self, caemra_id, track_id):
+        try:
+            self.td[caemra_id][track_id]
+        except KeyError:
+            return None
+
+    def serialize(self):
+        d = dict()
+        d['td'] = self.td
+        d['kp_l0']=self.kp_l0
+        d['kp_r0'] = self.kp_r0
+        d['first_frame_ind'] = self.first_frame_ind
+        datetime = strftime("%d_%m_%H_%M", gmtime())
+        path = os.path.join(TRACKS_PATH,datetime+'.pickle')
+        with open(path, 'wb') as handle:
+            pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def read(path):
+        with open(path, 'rb') as handle:
+            d = pickle.load(handle)
+        return Tracks(kp_l0=d['kp_l0'],kp_r0=d['kp_r0'], first_frame_ind=d['first_frame_ind'], td=d['td'])
